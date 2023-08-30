9141b388cb966bcb81465a23acfcd23c743bcd1a
BorisDog <BorisDog@users.noreply.github.com>
2023-08-29 19:53:10 -0700

    Update DriverVersionHelper.cs

--- tests/MongoDB.Analyzer.Tests/Infrastructure/DriverVersionHelper.cs
+++ tests/MongoDB.Analyzer.Tests/Infrastructure/DriverVersionHelper.cs
@@ -21,10 +21,7 @@ internal static class DriverVersionHelper
 {
     public static readonly NuGetVersion[] DriverVersions = new[]
     {
-            NuGetVersion.Parse("2.12.4"),
-            NuGetVersion.Parse("2.14.1"),
-            NuGetVersion.Parse("2.17.1"),
-            NuGetVersion.Parse("2.18.0"),
+            NuGetVersion.Parse("2.18.0")
         };
 
     public static NuGetVersion[] FilterVersionForRange(string versionRange)
