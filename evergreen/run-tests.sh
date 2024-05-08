8234eeb3f77f06b342f5f3739f674c1970b1a13a
BorisDog <boris.dogad@gmail.com>
2024-05-08 15:32:35 -0700

    - fix

--- evergreen/run-tests.sh
+++ evergreen/run-tests.sh
@@ -5,10 +5,11 @@ dotnet clean "./MongoDB.Analyzer.sln"
 
 if [ "${DRIVER_VERSION}" == "latest" ]; then
     echo "Getting latest driver version from MyGet"	
-    sudo apt-get --assume-yes install jq
+    #sudo apt-get --assume-yes install jq
 
-    DRIVER_VERSION=$(curl "https://www.myget.org/F/mongodb/api/v3/query?prerelease=true&take=1&q=PackageId:MongoDB.Driver" | jq ".data[0].version")
+    $DRIVER_VERSION=$(curl "https://www.myget.org/F/mongodb/api/v3/query?prerelease=true&take=1&q=PackageId:MongoDB.Driver" | jq ".data[0].version")
     echo Latest driver version in MyGet: "$DRIVER_VERSION"
+    echo "-e DRIVER_VERSION=${DRIVER_VERSION}"
 fi;
 
 dotnet build "./tests/MongoDB.Analyzer.Tests.Common.ClassLibrary" -f netstandard2.0 -c Debug
