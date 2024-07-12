a153b548e94c13bed279b8401f1bd1abefabab56
Ravi Raghavan <ravi.raghavan@mongodb.com>
2024-07-11 21:29:52 -0400

    Cleanup DiagnosticsAnalyzer

--- tests/MongoDB.Analyzer.Tests/Infrastructure/DiagnosticsAnalyzer.cs
+++ tests/MongoDB.Analyzer.Tests/Infrastructure/DiagnosticsAnalyzer.cs
@@ -40,8 +40,6 @@ internal static class DiagnosticsAnalyzer
 #elif NETCOREAPP3_1
         var netReferences = ReferenceAssemblies.NetCore.NetCoreApp31;
 #endif
-        var assemblies = ImmutableArray.Create(PathUtilities.TestDataModelAssemblyPath);
-
         var packages = ImmutableArray.Create(new PackageIdentity("MongoDB.Driver", driverVersion));
         var EFPackage = ImmutableArray.Create(new PackageIdentity("Microsoft.EntityFrameworkCore", "3.1.0"));
 
