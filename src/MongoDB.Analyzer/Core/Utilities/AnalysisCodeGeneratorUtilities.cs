bb5c08deb7d51d476ff35407c302254b1ef3d80d
Ravi Raghavan <ravi.raghavan@mongodb.com>
2024-07-25 16:12:34 -0400

    Made suggested PR Changes

--- src/MongoDB.Analyzer/Core/Utilities/AnalysisCodeGeneratorUtilities.cs
+++ src/MongoDB.Analyzer/Core/Utilities/AnalysisCodeGeneratorUtilities.cs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 using MongoDB.Analyzer.Core.HelperResources;
+using NuGet.Versioning;
 
 namespace MongoDB.Analyzer.Core.Utilities;
 
@@ -20,47 +21,32 @@ internal static class AnalysisCodeGeneratorUtilities
 {
     private const string AnalysisAssemblyNameDRIVER_2_27_OR_Lower = "DynamicProxyGenAssembly2";
     private const string AnalysisAssemblyNameDRIVER_2_28_OR_Greater = "MongoDB.Analyzer.MQLGenerator";
+    private const string Driver_V2_28_OrGreater = "[2.28.0, )";
 
     public static Type CompileAndGetGeneratorType(AnalysisType analysisType, MongoAnalysisContext context, ReferencesContainer referencesContainer, IEnumerable<SyntaxTree> syntaxTrees)
     {
         var publicKey = new byte[160] { 0, 36, 0, 0, 4, 128, 0, 0, 148, 0, 0, 0, 6, 2, 0, 0, 0, 36, 0, 0, 82, 83, 65, 49, 0, 4, 0, 0, 1, 0, 1, 0, 53, 40, 127, 13, 56, 131, 192, 160, 117, 200, 142, 12, 218, 60, 233, 59, 98, 16, 3, 236, 189, 94, 146, 13, 74, 140, 114, 56, 86, 79, 77, 47, 79, 104, 17, 106, 202, 40, 201, 178, 19, 65, 220, 58, 135, 118, 121, 193, 69, 86, 25, 43, 43, 47, 95, 226, 193, 29, 98, 78, 8, 148, 211, 8, 255, 123, 148, 191, 111, 215, 42, 239, 27, 65, 1, 127, 254, 37, 114, 233, 144, 25, 209, 198, 25, 99, 230, 140, 208, 237, 103, 115, 74, 66, 203, 51, 59, 128, 142, 56, 103, 203, 230, 49, 147, 114, 20, 227, 46, 64, 159, 177, 250, 98, 253, 182, 157, 73, 76, 37, 48, 230, 74, 64, 228, 23, 214, 238 };
-        var compilation = CSharpCompilation.Create(AnalysisAssemblyNameDRIVER_2_28_OR_Greater, syntaxTrees, referencesContainer.References, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, publicSign: true, cryptoPublicKey: publicKey.ToImmutableArray()));
+        var driverVersion = referencesContainer.Version.ToString();
 
-        using var memoryStream_Driver_2_28_Or_Greater = new MemoryStream();
-        var emitResult = compilation.Emit(memoryStream_Driver_2_28_Or_Greater);
+        var assemblyName = IsDriverVersion_2_28_Or_Greater(driverVersion) ? AnalysisAssemblyNameDRIVER_2_28_OR_Greater : AnalysisAssemblyNameDRIVER_2_27_OR_Lower;
+        var compilation = CSharpCompilation.Create(assemblyName, syntaxTrees, referencesContainer.References, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, publicSign: true, cryptoPublicKey: publicKey.ToImmutableArray()));
+
+        using var memoryStream = new MemoryStream();
+        var emitResult = compilation.Emit(memoryStream);
 
         Type generatorType = null;
 
         if (emitResult.Success)
         {
-            context.Logger.Log("Compilation successful for AnalysisAssemblyName for 2.28 Driver Versions and Above");
+            context.Logger.Log("Compilation successful");
 
-            memoryStream_Driver_2_28_Or_Greater.Seek(0, SeekOrigin.Begin);
+            memoryStream.Seek(0, SeekOrigin.Begin);
 
-            generatorType = DynamicTypeProvider.GetType(referencesContainer, memoryStream_Driver_2_28_Or_Greater, GetGeneratorFullName(analysisType));
+            generatorType = DynamicTypeProvider.GetType(referencesContainer, memoryStream, GetGeneratorFullName(analysisType));
         }
         else
         {
-            context.Logger.Log($"Compilation failed with AnalysisAssemblyName for 2.28 Driver Versions and Above : {string.Join(Environment.NewLine, emitResult.Diagnostics)}");
-
-            compilation = CSharpCompilation.Create(AnalysisAssemblyNameDRIVER_2_27_OR_Lower, syntaxTrees, referencesContainer.References, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, publicSign: true, cryptoPublicKey: publicKey.ToImmutableArray()));
-
-            using var memoryStream_Driver_2_27_Or_Lower = new MemoryStream();
-            emitResult = compilation.Emit(memoryStream_Driver_2_27_Or_Lower);
-
-            if (emitResult.Success)
-            {
-                context.Logger.Log("Compilation successful for AnalysisAssemblyName with 2.27 Driver Versions And Below");
-
-                memoryStream_Driver_2_27_Or_Lower.Seek(0, SeekOrigin.Begin);
-
-                generatorType = DynamicTypeProvider.GetType(referencesContainer, memoryStream_Driver_2_27_Or_Lower, GetGeneratorFullName(analysisType));
-
-            }
-            else
-            {
-                context.Logger.Log($"Compilation failed with AnalysisAssemblyName for 2.27 Driver Versions And Below : {string.Join(Environment.NewLine, emitResult.Diagnostics)}");
-            }
+            context.Logger.Log($"Compilation failed with: {string.Join(Environment.NewLine, emitResult.Diagnostics)}");
         }
 
         return generatorType;
@@ -74,5 +60,7 @@ internal static class AnalysisCodeGeneratorUtilities
             AnalysisType.Poco => JsonSyntaxElements.Poco.JsonGeneratorFullName,
             _ => throw new ArgumentOutOfRangeException(nameof(analysisType), analysisType, "Unsupported analysis type")
         };
+
+    private static bool IsDriverVersion_2_28_Or_Greater(string driverVersion) => VersionRange.Parse(Driver_V2_28_OrGreater).Satisfies(NuGetVersion.Parse(driverVersion));
 }
 
--- tests/MongoDB.Analyzer.Tests/Infrastructure/DiagnosticsAnalyzer.cs
+++ tests/MongoDB.Analyzer.Tests/Infrastructure/DiagnosticsAnalyzer.cs
@@ -12,13 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
 using System.Collections.Immutable;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Metadata;
-using System.Reflection.PortableExecutable;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -61,10 +56,8 @@ internal static class DiagnosticsAnalyzer
         var testCodeText = File.ReadAllText(testCodeFilename);
         var testCodeSyntaxTree = CSharpSyntaxTree.ParseText(testCodeText);
 
-        var publicKey = new byte[160] { 0, 36, 0, 0, 4, 128, 0, 0, 148, 0, 0, 0, 6, 2, 0, 0, 0, 36, 0, 0, 82, 83, 65, 49, 0, 4, 0, 0, 1, 0, 1, 0, 53, 40, 127, 13, 56, 131, 192, 160, 117, 200, 142, 12, 218, 60, 233, 59, 98, 16, 3, 236, 189, 94, 146, 13, 74, 140, 114, 56, 86, 79, 77, 47, 79, 104, 17, 106, 202, 40, 201, 178, 19, 65, 220, 58, 135, 118, 121, 193, 69, 86, 25, 43, 43, 47, 95, 226, 193, 29, 98, 78, 8, 148, 211, 8, 255, 123, 148, 191, 111, 215, 42, 239, 27, 65, 1, 127, 254, 37, 114, 233, 144, 25, 209, 198, 25, 99, 230, 140, 208, 237, 103, 115, 74, 66, 203, 51, 59, 128, 142, 56, 103, 203, 230, 49, 147, 114, 20, 227, 46, 64, 159, 177, 250, 98, 253, 182, 157, 73, 76, 37, 48, 230, 74, 64, 228, 23, 214, 238 };
-        var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, publicSign: true, cryptoPublicKey: publicKey.ToImmutableArray());
-        var assemblyName = isDriverVersion_2_28_Or_Greater ? "DynamicProxyGenAssembly2" : "TestAssembly";
-        var compilation = CSharpCompilation.Create(assemblyName, new[] { testCodeSyntaxTree }, metadataReferences, compilationOptions);
+        var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+        var compilation = CSharpCompilation.Create("TestAssembly", new[] { testCodeSyntaxTree }, metadataReferences, compilationOptions);
 
         var mongodbAnalyzer = new MongoDBDiagnosticAnalyzer();
         var linqDefaultVersion = linqVersion == Common.LinqVersion.Undefined ? null : (LinqVersion?)linqVersion;
@@ -78,79 +71,8 @@ internal static class DiagnosticsAnalyzer
         var analyzerOptions = new AnalyzerOptions(ImmutableArray.Create<AdditionalText>(new AdditionalTextAnalyzerSettings(settings)));
 
         var compilationWithAnalyzers = compilation.WithAnalyzers(ImmutableArray.Create<DiagnosticAnalyzer>(mongodbAnalyzer), analyzerOptions);
-
-        var test = PrintAssemblyVersion(compilation, "MongoDB.Bson");
-        var test2 = PrintSpecificReferencedAssemblyVersion($"{testDataModelAssembly}.dll", "MongoDB.Bson");
-        var test3 = PrintSpecificReferencedAssemblyVersion($"{testDataModelAssembly}.dll", "MongoDB.Driver");
-
         var diagnostics = await compilationWithAnalyzers.GetAllDiagnosticsAsync();
 
         return diagnostics;
-    }
-
-    public static string PrintAssemblyVersion(CSharpCompilation compilation, string assemblyName)
-    {
-        var assemblyReference = compilation.References
-        .OfType<PortableExecutableReference>()
-        .FirstOrDefault(r => Path.GetFileNameWithoutExtension(r.FilePath).Equals(assemblyName, StringComparison.OrdinalIgnoreCase));
-
-        if (assemblyReference != null)
-        {
-            // Read the assembly from the file path
-            using var stream = new FileStream(assemblyReference.FilePath, FileMode.Open, FileAccess.Read);
-            var reader = new PEReader(stream);
-            var metadataReader = reader.GetMetadataReader();
-            var assemblyDefinition = metadataReader.GetAssemblyDefinition();
-            var version = assemblyDefinition.Version;
-
-            return $"Assembly: {assemblyName}, Version: {version}";
-        }
-        else
-        {
-            return $"Assembly: {assemblyName} not found in the compilation references.";
-        }
-    }
-
-    public static string PrintAssemblyVersion2(string dllPath)
-    {
-        try
-        {
-            var assembly = Assembly.LoadFrom(dllPath);
-            var version = assembly.GetName().Version;
-            return $"Assembly version: {version}";
-        }
-        catch (Exception ex)
-        {
-            return $"Failed to load assembly: {ex.Message}";
-        }
-    }
-
-    public static string PrintSpecificReferencedAssemblyVersion(string dllPath, string targetAssemblyName)
-    {
-        try
-        {
-            using var stream = new FileStream(dllPath, FileMode.Open, FileAccess.Read);
-            var peReader = new PEReader(stream);
-            var metadataReader = peReader.GetMetadataReader();
-
-            foreach (var reference in metadataReader.AssemblyReferences)
-            {
-                var assemblyReference = metadataReader.GetAssemblyReference(reference);
-                var name = metadataReader.GetString(assemblyReference.Name);
-
-                // Check if the name matches the target assembly
-                if (name.Equals(targetAssemblyName, StringComparison.OrdinalIgnoreCase))
-                {
-                    var version = assemblyReference.Version;
-                    return $"Assembly: {name}, Version: {version}";
-                }
-            }
-
-            return $"Assembly: {targetAssemblyName} not found in the references.";
-        }
-        catch (Exception ex)
-        {
-            return $"Failed to read assembly: {ex.Message}";
-        }
     }
 }
